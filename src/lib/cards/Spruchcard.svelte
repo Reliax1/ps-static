98539b48a435e4b6050f485b4670b13bb2da25db
Andreas L <reliax@gmx.de>
2023-02-03 21:58:34 +0100

    google hotfix5

--- src/lib/cards/Spruchcard.svelte
+++ src/lib/cards/Spruchcard.svelte
@@ -884,11 +884,11 @@
 		}
 		.spruchcard-image-container {
 			width: 100%;
-			height: 500px; //500px // 500px
+			height: 59%; //500px // 500px
 		}
 		.spruchcard-text-container {
 			width: 100%;
-			height: 295.6px; //400px // 295.6px
+			height: 41%; //400px // 295.6px
 			border-bottom-left-radius: 10px;
 			border-bottom-right-radius: 10px;
 		}
