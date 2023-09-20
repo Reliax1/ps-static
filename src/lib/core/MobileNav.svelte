d8c1bac8144e684d31f83fc6d3620395f1451679
Andreas L <reliax@gmx.de>
2023-09-20 04:44:44 +0200

    add mobile banner2

--- src/lib/core/MobileNav.svelte
+++ src/lib/core/MobileNav.svelte
@@ -42,12 +42,14 @@
 >
 	{#if $HelperStore.isMobile === true && $HelperStore.mediaType === 'google'}
 		<div class="media-mobile-wrapper">
-			<ins
-				class="adsbygoogle"
-				style="display:inline-block;width:300px;height:50px"
-				data-ad-client="ca-pub-6800691774097678"
-				data-ad-slot="1381512858"
-			/>
+			<div class="media-mobile">
+				<ins
+					class="adsbygoogle"
+					style="display:inline-block;width:300px;height:50px"
+					data-ad-client="ca-pub-6800691774097678"
+					data-ad-slot="1381512858"
+				/>
+			</div>
 		</div>
 	{/if}
 
@@ -227,11 +229,14 @@
 	.media-mobile-wrapper {
 		width: 100%;
 		height: 50px;
-		// background-color: grey;
 		z-index: 900;
 		display: flex;
 		justify-content: center;
 		align-items: center;
+	}
+	.media-mobile {
+		width: 300px;
+		height: 50px;
 	}
 
 	.mobilenav1 {
