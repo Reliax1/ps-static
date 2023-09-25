380c1db0922aaff3f08a70dcd7ca304e0df2c4b3
Andreas L <reliax@gmx.de>
2023-09-25 15:25:03 +0200

    25.09.2023 7

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -89,9 +89,10 @@
 		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6800691774097678"
 		crossorigin="anonymous"></script>
 
-	<script>
+	<!-- <script>
 		(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
+	</script> -->
+	
 	<!-- Google tag (gtag.js) -->
 	<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-7PT3JH3660"></script>
 	<script>
