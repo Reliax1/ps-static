67244d37d781cdfd3175f939a4bce7bcdeb9ba74
Andreas L <reliax@gmx.de>
2023-11-07 03:00:38 +0100

    07.11.2023 3

--- src/lib/lazymedia/Mainmedia.svelte
+++ src/lib/lazymedia/Mainmedia.svelte
@@ -165,7 +165,7 @@
 {:else if source === 'desktopsticky' && isMobile === false}
 	<div class="yellow-wrapper-desktop">
 		<div class="anz-desktop">
-			<div id="ezoic-pub-ad-placeholder-103" />
+			<div id="ezoic-pub-ad-placeholder-{placeholder}" />
 		</div>
 	</div>
 {:else if source === 'mobileBanner' && isMobile === true}
