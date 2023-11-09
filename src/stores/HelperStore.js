0700d057235392e40cb4308fdfde6e8b10bfd19b
Andreas L <reliax@gmx.de>
2023-11-09 06:02:46 +0100

    09.11.2023 19

--- src/stores/HelperStore.js
+++ src/stores/HelperStore.js
@@ -11,7 +11,7 @@ const HelperStore = writable({
 	placeholder: {
 		mobile_inline: 130,
 		desktopsticky: 103,
-		mobileBanner: null, // 106
+		mobileBanner: 106, // 106
 		desktopInline: 147,
 		tablet: 141,
 		define1: null,
