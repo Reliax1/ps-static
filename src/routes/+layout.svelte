f4d5944e24b23ca3dc539a301a0546fcd259c07e
Andreas L <reliax@gmx.de>
2023-11-22 22:07:32 +0100

    22.11.2023 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -128,7 +128,7 @@
 		function EzConsentCallback(consent) {
 			// consent contains the necessary, preferences, statistics, and marketing properties with boolean values
 			if (consent.marketing) {
-				console.log('check111111111');
+				console.log('check');
 			}
 		}
 	</script>
